48a81b8ab357167425b1acedb20e9eebb83ae8ad
lunjj@create80.com <lunjj@create80.com>
2020-01-17 14:24:16 +0800

    测试CI

--- app/controller/news.js
+++ app/controller/news.js
@@ -5,7 +5,7 @@ class NewController extends Controller {
     const user = await this.service.newsService.find();
     console.log(user);
     this.ctx.body = {
-      user: 'admin—test1',
+      user: 'admin—test2',
       data: user,
     };
   }
